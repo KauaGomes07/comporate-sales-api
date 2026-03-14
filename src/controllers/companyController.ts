5198e21d29db125eb5d4f4dff71b6747a1fb3a0f
Kauã Oliver <kauagomesoliveira321@gmail.com>
2026-03-14 09:28:01 -0300

    Criação da rota get para o usuário conseguir listar a empresa que está associado.

--- src/controllers/companyController.ts
+++ src/controllers/companyController.ts
@@ -5,9 +5,14 @@ export const createCompany = async (req: Request, res: Response) => {
     try {
         const { name } = req.body
         const userId = req.user?.id
+
         //Verifico se foi passado o nome da empresa
         if (!name){
             return res.status(400).json({ message: "Nome da empresa é obrigatório" })
+        }
+
+        if (!userId) {
+            return res.status(401).json({ message: "Usuário não autenticado" })
         }
         //Crio a empresa
         const company = await prisma.company.create({
@@ -28,5 +33,32 @@ export const createCompany = async (req: Request, res: Response) => {
     catch (err) {
         console.error(err)
         return res.status(500).json({ message: "Erro ao criar empresa" })
+    }
+}
+
+export const getMyCompany = async (req: Request, res: Response) => {
+    try {
+
+        const companyId = req.user?.companyId
+
+        if (!companyId){
+            return res.status(403).json({ message: "Usuário não pertence a uma empresa" })
+        }
+
+        const company = await prisma.company.findUnique({
+            where: { id: companyId },
+            include: {
+                products: true
+            }
+        })
+        if (!company){
+            return res.status(404).json({ message: "Empresa não encontrada" })
+        }
+
+        return res.status(200).json(company)
+    }
+    catch (err) {
+        console.error(err)
+        return res.status(500).json({ message: "Erro ao buscar empresa" })
     }
 }
